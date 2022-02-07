3eb2b8ffed8c37a18ce83579a83eb4e99393b37c
norhap <elfogondesegovia@gmail.com>
2022-02-07 20:43:54 +0100

    files python identation to tabs

--- usr/lib/enigma2/python/Components/Converter/BTVInfo.py
+++ usr/lib/enigma2/python/Components/Converter/BTVInfo.py
@@ -7,131 +7,131 @@ from Poll import Poll
 from enigma import getBoxType
 
 class BTVInfo(Poll, Converter, object):
-    BOXTYPE = 0
-    LOAD = 1
-    MEMINFO = 2
-    FREEFLASH = 3
-    UPTIME = 4
-    TEMPSENSOR = 5
+	BOXTYPE = 0
+	LOAD = 1
+	MEMINFO = 2
+	FREEFLASH = 3
+	UPTIME = 4
+	TEMPSENSOR = 5
 
-    def __init__(self, type):
-        Converter.__init__(self, type)
-        Poll.__init__(self)
-        self.poll_interval = 10000
-        self.poll_enabled = True
-        if type == 'BoxType':
-            self.type = self.BOXTYPE
-            self.poll_enabled = False
-        elif type == 'LoadAverage':
-            self.type = self.LOAD
-        elif type == 'MemInfo':
-            self.type = self.MEMINFO
-        elif type == 'FreeFlash':
-            self.type = self.FREEFLASH
-        elif type == 'TemperaturaSensor':
-            self.type = self.TEMPSENSOR
-        elif type == 'Actividad':
-            self.type = self.UPTIME
-        else:
-            self.type = self.BOXTYPE
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		Poll.__init__(self)
+		self.poll_interval = 10000
+		self.poll_enabled = True
+		if type == 'BoxType':
+			self.type = self.BOXTYPE
+			self.poll_enabled = False
+		elif type == 'LoadAverage':
+			self.type = self.LOAD
+		elif type == 'MemInfo':
+			self.type = self.MEMINFO
+		elif type == 'FreeFlash':
+			self.type = self.FREEFLASH
+		elif type == 'TemperaturaSensor':
+			self.type = self.TEMPSENSOR
+		elif type == 'Actividad':
+			self.type = self.UPTIME
+		else:
+			self.type = self.BOXTYPE
 
-    def getModel(self):
-        try:
-            box_info = getBoxType()
-        except:
-            return 'Model: N/A'
-            box_info = None
+	def getModel(self):
+		try:
+			box_info = getBoxType()
+		except:
+			return 'Model: N/A'
+			box_info = None
 
-        if box_info is not None:
-            return 'Dreambox %s' % box_info
+		if box_info is not None:
+			return 'Dreambox %s' % box_info
 
-    def getLoadAverage(self):
-        try:
-            with open('/proc/loadavg', 'r') as file:
-                load_info = file.read().split()[0:3]
-        except:
-            return 'Load average: N/A'
-            load_info = None
+	def getLoadAverage(self):
+		try:
+			with open('/proc/loadavg', 'r') as file:
+				load_info = file.read().split()[0:3]
+		except:
+			return 'Load average: N/A'
+			load_info = None
 
-        if load_info is not None:
-            return 'Load average: %s' % ', '.join(load_info)
+		if load_info is not None:
+			return 'Load average: %s' % ', '.join(load_info)
 
-    def getMemInfo(self):
-        try:
-            with open('/proc/meminfo', 'r') as file:
-                mem_info = int(file.read().split()[4])
-        except:
-            return 'MemFree: N/A'
-            mem_info = None
+	def getMemInfo(self):
+		try:
+			with open('/proc/meminfo', 'r') as file:
+				mem_info = int(file.read().split()[4])
+		except:
+			return 'MemFree: N/A'
+			mem_info = None
 
-        if mem_info is not None:
-            return 'MemFree: %s MB' % (mem_info / 1024)
+		if mem_info is not None:
+			return 'MemFree: %s MB' % (mem_info / 1024)
 
-    def getFreeFlash(self):
-        try:
-            flash_info = os.statvfs('/')
-        except:
-            return 'FlashFree: N/A'
-            flash_info = None
+	def getFreeFlash(self):
+		try:
+			flash_info = os.statvfs('/')
+		except:
+			return 'FlashFree: N/A'
+			flash_info = None
 
-        if flash_info is not None:
-            free_flash = int(flash_info.f_frsize * flash_info.f_bavail / 1024 / 1024)
-            return 'FlashFree: %s MB' % free_flash
+		if flash_info is not None:
+			free_flash = int(flash_info.f_frsize * flash_info.f_bavail / 1024 / 1024)
+			return 'FlashFree: %s MB' % free_flash
 
-    def getUptime(self):
-        try:
-            with open('/proc/uptime', 'r') as file:
-                uptime_info = file.read().split()
-        except:
-            return 'Funcionamiento: N/A'
-            uptime_info = None
+	def getUptime(self):
+		try:
+			with open('/proc/uptime', 'r') as file:
+				uptime_info = file.read().split()
+		except:
+			return 'Funcionamiento: N/A'
+			uptime_info = None
 
-        if uptime_info is not None:
-            total_seconds = float(uptime_info[0])
-            MINUTE = 60
-            HOUR = MINUTE * 60
-            DAY = HOUR * 24
-            days = int(total_seconds / DAY)
-            hours = int(total_seconds % DAY / HOUR)
-            minutes = int(total_seconds % HOUR / MINUTE)
-            seconds = int(total_seconds % MINUTE)
-            uptime = ''
-            if days > 0:
-                uptime += str(days) + ' ' + (days == 1 and 'dia' or 'dias') + ', '
-            if len(uptime) > 0 or hours > 0:
-                uptime += str(hours) + ' ' + (hours == 1 and 'hora' or 'horas') + ', '
-            if len(uptime) > 0 or minutes > 0:
-                uptime += str(minutes) + ' ' + (minutes == 1 and 'minuto' or 'minutos')
-            return 'Funcionamiento Receptor: %s' % uptime
+		if uptime_info is not None:
+			total_seconds = float(uptime_info[0])
+			MINUTE = 60
+			HOUR = MINUTE * 60
+			DAY = HOUR * 24
+			days = int(total_seconds / DAY)
+			hours = int(total_seconds % DAY / HOUR)
+			minutes = int(total_seconds % HOUR / MINUTE)
+			seconds = int(total_seconds % MINUTE)
+			uptime = ''
+			if days > 0:
+				uptime += str(days) + ' ' + (days == 1 and 'dia' or 'dias') + ', '
+			if len(uptime) > 0 or hours > 0:
+				uptime += str(hours) + ' ' + (hours == 1 and 'hora' or 'horas') + ', '
+			if len(uptime) > 0 or minutes > 0:
+				uptime += str(minutes) + ' ' + (minutes == 1 and 'minuto' or 'minutos')
+			return 'Funcionamiento Receptor: %s' % uptime
 
-    def getTempSensor(self):
-        if getBoxType() not in ("dm7020hd","dm7020hdv2"):
-            try:
-                sensor_info = sensors.getSensorsList(sensors.TYPE_TEMPERATURE)
-            except:
-                return 'Temperatura: N/A'
-                sensor_info = None
+	def getTempSensor(self):
+		if getBoxType() not in ("dm7020hd","dm7020hdv2"):
+			try:
+				sensor_info = sensors.getSensorsList(sensors.TYPE_TEMPERATURE)
+			except:
+				return 'Temperatura: N/A'
+				sensor_info = None
 
-            if sensor_info is not None:
-                if len(sensor_info) > 0:
-                    return 'Temperatura: %s\xc2\xb0C' % sensors.getSensorValue(sensor_info[0])
-        return 'Temperatura: No Sensor'
+			if sensor_info is not None:
+				if len(sensor_info) > 0:
+					return 'Temperatura: %s\xc2\xb0C' % sensors.getSensorValue(sensor_info[0])
+		return 'Temperatura: No Sensor'
 
-    @cached
-    def getText(self):
-        if self.type == self.BOXTYPE:
-            return self.getModel()
-        elif self.type == self.TEMPSENSOR:
-            return self.getTempSensor()
-        elif self.type == self.LOAD:
-            return self.getLoadAverage()
-        elif self.type == self.MEMINFO:
-            return self.getMemInfo()
-        elif self.type == self.FREEFLASH:
-            return self.getFreeFlash()
-        elif self.type == self.UPTIME:
-            return self.getUptime()
-        else:
-            return '???'
+	@cached
+	def getText(self):
+		if self.type == self.BOXTYPE:
+			return self.getModel()
+		elif self.type == self.TEMPSENSOR:
+			return self.getTempSensor()
+		elif self.type == self.LOAD:
+			return self.getLoadAverage()
+		elif self.type == self.MEMINFO:
+			return self.getMemInfo()
+		elif self.type == self.FREEFLASH:
+			return self.getFreeFlash()
+		elif self.type == self.UPTIME:
+			return self.getUptime()
+		else:
+			return '???'
 
-    text = property(getText)
+	text = property(getText)
